8409f50865ed24541e14e7e286ac1c0f57d6de28
Dmitriy Virych <dmitry.virych@requestum.com>
2017-06-01 16:08:10 +0300

    Initial Commit

---
+++ DVCaptureView/DVCaptureView.h
@@ -0,0 +1,19 @@
+//
+//  DVCaptureView.h
+//  DVCaptureView
+//
+//  Created by Dmitriy Virych on 6/1/17.
+//  Copyright © 2017 DmitryVirych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DVCaptureView.
+FOUNDATION_EXPORT double DVCaptureViewVersionNumber;
+
+//! Project version string for DVCaptureView.
+FOUNDATION_EXPORT const unsigned char DVCaptureViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DVCaptureView/PublicHeader.h>
+
+
